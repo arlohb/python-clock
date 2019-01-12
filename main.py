7f57ede4a7c338455b28b7f092042f143d7e97d5
Arlo Blythe <arlo@urwick.co.uk>
2019-01-12 08:26:43 +0000

    prints date and time

--- main.py
+++ main.py
@@ -2,12 +2,23 @@ import time
 
 months=["January","February","March","April","May","June","July","August","September","October""November","December"]
 
-timeStruct=time.localtime(time.time())
+while True:
+	timeStruct=time.localtime(time.time())
 
-month=months[timeStruct.tm_mon-1]
-year=str(timeStruct.tm_year)
-dayNum=str(timeStruct.tm_mday)
-
-#print(mday,mon,year)
-print(dayNum+", "+month+", "+year)
-#print(hour,min,secs)
+	month=months[timeStruct.tm_mon-1]
+	year=str(timeStruct.tm_year)
+	dayNum=str(timeStruct.tm_mday)
+	hour=str(timeStruct.tm_hour)
+	min=str(timeStruct.tm_min)
+	secs=str(timeStruct.tm_sec)
+	
+	if(len(hour)==1):
+		hour="0"+hour
+	if(len(min)==1):
+		min="0"+min
+	if(len(secs)==1):
+		secs="0"+secs
+	
+	print(dayNum+", "+month+", "+year)
+	print(hour+":"+min+":"+secs)#
+	time.sleep(1)
